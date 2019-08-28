e4fe7f0b1713991659e0249c4f5272acf0f73781
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-08-28 09:44:44 +0200

    Tilføjede klasser og interfaces til Core

---
+++ PetShop/PetShop.Core.Entity/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.Entity
+{
+    public class Pet
+    {
+        int id { get; set; }
+        string name { get; set; }
+        enum type { Dog, Cat, Snake, BeardedDragon, Elephant, Goat}
+        DateTime birthDate { get; set; }
+        DateTime soldDate { get; set; }
+        string color { get; set; }
+        string previousOwner { get; set; }
+        double price { get; set; }
+
+
+    }
+}
--- PetShop/PetShop.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PetShop.Core
-{
-    public class Class1
-    {
-    }
-}
