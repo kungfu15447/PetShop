58b3a815c43bd399cca87c59ea7a807f9e14380e
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-08-29 14:51:10 +0200

    Tilføjede nogle flere dyr til FakeDB klassen

--- PetShop/PetShop.InfraStructure.Data/FakeDB.cs
+++ PetShop/PetShop.InfraStructure.Data/FakeDB.cs
@@ -49,7 +49,79 @@ namespace PetShop.InfraStructure.Data
                 price = 350
             };
 
-            petList = new List<Pet> { pet, pet2, pet3 };
+            Pet pet4 = new Pet
+            {
+                id = id++,
+                name = "Dox",
+                type = PetTypes.Dog,
+                birthDate = new DateTime(2016, 9, 18),
+                soldDate = new DateTime(2017, 12, 15),
+                color = "Brown",
+                previousOwner = "Poop",
+                price = 675
+            };
+
+            Pet pet5 = new Pet
+            {
+                id = id++,
+                name = "Foo",
+                type = PetTypes.Cat,
+                birthDate = new DateTime(2016, 10, 18),
+                soldDate = new DateTime(2017, 2, 15),
+                color = "A little red",
+                previousOwner = "Sørgen Høgh Henreiksen",
+                price = 2200
+            };
+
+            Pet pet6 = new Pet
+            {
+                id = id++,
+                name = "Kentuck",
+                type = PetTypes.Goat,
+                birthDate = new DateTime(2000, 8, 6),
+                soldDate = new DateTime(2001, 3, 20),
+                color = "White",
+                previousOwner = "Engötz Löyrizen",
+                price = 200
+            };
+
+            Pet pet7 = new Pet
+            {
+                id = id++,
+                name = "Maximus",
+                type = PetTypes.Dog,
+                birthDate = new DateTime(2010, 12, 30),
+                soldDate = new DateTime(2011, 2, 20),
+                color = "Pure black",
+                previousOwner = "None",
+                price = 700
+            };
+
+            Pet pet8 = new Pet
+            {
+                id = id++,
+                name = "Goop",
+                type = PetTypes.BeardedDragon,
+                birthDate = new DateTime(2012, 10, 18),
+                soldDate = new DateTime(2015, 2, 15),
+                color = "Colourful",
+                previousOwner = "none",
+                price = 3500
+            };
+
+            Pet pet9 = new Pet
+            {
+                id = id++,
+                name = "Pog",
+                type = PetTypes.Elephant,
+                birthDate = new DateTime(2017, 10, 7),
+                soldDate = new DateTime(2018, 4, 21),
+                color = "Orange and a little brown",
+                previousOwner = "Karl Larsen",
+                price = 5000
+            };
+
+            petList = new List<Pet> { pet, pet2, pet3, pet4, pet5, pet6, pet7, pet8, pet9};
             
         }
     }
